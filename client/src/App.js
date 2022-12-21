5707733b8c483348c1f5849487f6ae9671b01409
melissaAcheng <melcheng12@gmail.com>
2022-12-21 11:22:20 -0500

    added disable dev tool dependency and updated App.js get url

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:8000/api/users", { withCredentials: true })
+      .get("https://studio-portal.onrender.com", { withCredentials: true })
       .then((res) => {
         console.log(res.data);
         if (res.data != null) {
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+if (process.env.NODE_ENV === "production") disableReactDevTools();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
