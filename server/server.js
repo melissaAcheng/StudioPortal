577cf65b307affd86179c0939bd2063ac401b2ce
melissaAcheng <melcheng12@gmail.com>
2022-12-21 15:05:47 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://studio-portal.onrender.com",
 		credentials: true,
 	})
 );
