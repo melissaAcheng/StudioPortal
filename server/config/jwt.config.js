bc8abe7d31ed3633d6c055ce34aa4bb4fe50f63d
melissaAcheng <melcheng12@gmail.com>
2023-01-25 18:10:27 -0500

    debugging jwt

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -1,19 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = {
-  authenticate(req, res, next) {
-    jwt.verify(req.cookies.usertoken, 
-      process.env.JWT_SECRET, 
-      (err, payload) => {
-        if (err) {
-          console.log(err);
-          res.status(401).json({ verified: false });
-        } else {
-          console.log(payload)
-          req.jwtpayload = payload
+	authenticate(req, res, next) {
+		console.log("check for jwt cookies", req.cookies);
+		jwt.verify(req.cookies.usertoken, process.env.JWT_SECRET, (err, payload) => {
+			if (err) {
+				console.log(err);
+				res.status(401).json({ verified: false });
+			} else {
+				console.log(payload);
+				req.jwtpayload = payload;
 
-          next()
-        }
-    });
-  },
+				next();
+			}
+		});
+	},
 };
