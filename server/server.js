3ff6b9b853c8b5893cea496cdba873f2791875a6
melissaAcheng <melcheng12@gmail.com>
2023-02-01 12:54:16 -0500

    built frontend

--- server/server.js
+++ server/server.js
@@ -41,8 +41,8 @@ mongoose
 
 // static files (build of your frontend)
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../client", "public")));
+	app.use(express.static(path.join(__dirname, "../client", "build")));
 	app.get("/*", (req, res) => {
-		res.sendFile(path.join(__dirname, "../client", "public", "index.html"));
+		res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
 	});
 }
