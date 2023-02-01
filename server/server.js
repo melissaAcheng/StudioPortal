002d235d7184731fecaf2a19c0c54792fe9af0b3
melissaAcheng <melcheng12@gmail.com>
2023-02-01 15:33:57 -0500

    allow origin with protocol

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: ["http://localhost:3000", "vercel.app", "studio-portal-beryl.vercel.app"],
+		origin: ["http://localhost:3000", "https://studio-portal-beryl.vercel.app"],
 		credentials: true,
 		exposedHeaders: ["usertoken"],
 	})
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Credentials", true);
-	res.header("Access-Control-Allow-Origin", ["vercel.app","studio-portal-beryl.vercel.app"]);
+	res.header("Access-Control-Allow-Origin", ["vercel.app", "studio-portal-beryl.vercel.app"]);
 	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,UPDATE,OPTIONS");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
 
