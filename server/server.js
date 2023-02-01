ebb6342c407b886da42ea4a8ba86d7adfcb9517b
melissaAcheng <melcheng12@gmail.com>
2023-02-01 15:34:20 -0500

    allow origin with protocol

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Credentials", true);
-	res.header("Access-Control-Allow-Origin", ["vercel.app", "studio-portal-beryl.vercel.app"]);
+	res.header("Access-Control-Allow-Origin", ["vercel.app", "https://studio-portal-beryl.vercel.app"]);
 	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,UPDATE,OPTIONS");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
 
